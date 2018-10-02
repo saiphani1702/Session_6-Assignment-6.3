c5b6b4ed74d74faca261fb5521c3a846c7a3e24c
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-02 15:43:57 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,6 @@
+var temperatureInCelsius=40;
+var celsiusToFahrenheit= (temperatureInCelsius*9)/5+32;
+console.log("Temperature of "+temperatureInCelsius+" celsius,will be "+celsiusToFahrenheit+" fahrenheit");
+var temperatureInFahrenheit=104;
+var fahreheitToCelsius=((temperatureInFahrenheit-32)*5)/9;
+console.log("Temperature of "+temperatureInFahrenheit+" fahrenheit,will be "+fahreheitToCelsius+" celsius");
