232c393898a6e516e42abd87ffbc4ff43945fe8a
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-02 16:57:12 +0530

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -1,6 +1,6 @@
-var temperatureInCelsius=40;
-var celsiusToFahrenheit= (temperatureInCelsius*9)/5+32;
-console.log("Temperature of "+temperatureInCelsius+" celsius,will be "+celsiusToFahrenheit+" fahrenheit");
-var temperatureInFahrenheit=104;
-var fahreheitToCelsius=((temperatureInFahrenheit-32)*5)/9;
-console.log("Temperature of "+temperatureInFahrenheit+" fahrenheit,will be "+fahreheitToCelsius+" celsius");
+var temperatureInCelsius=40;//initializing variables
+var celsiusToFahrenheit= (temperatureInCelsius*9)/5+32;//conversion from celsius to fahrenheit
+console.log("Temperature of "+temperatureInCelsius+" celsius,will be "+celsiusToFahrenheit+" fahrenheit");//displays output on the console
+var temperatureInFahrenheit=104;//initializing variables
+var fahreheitToCelsius=((temperatureInFahrenheit-32)*5)/9;//conversion from fahrenheit to celsius
+console.log("Temperature of "+temperatureInFahrenheit+" fahrenheit,will be "+fahreheitToCelsius+" celsius");//displays output on the console
